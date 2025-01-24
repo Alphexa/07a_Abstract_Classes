5d2399301208b4a84ad0062ddd51648cc7e06ad1
Alexander <alexander.pe.mobil@gmail.com>
2025-01-24 10:52:48 +0100

    6th change

--- com/cc/java/Rectangle.java
+++ com/cc/java/Rectangle.java
@@ -12,10 +12,12 @@ public class Rectangle extends Shape{
 
     @Override
     public double area() {
-        // TODO Auto-generated method stub
         return height * width;
     }
 
     
+  
+
+
 
 }
