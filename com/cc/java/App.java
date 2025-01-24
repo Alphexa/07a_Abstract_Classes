9d493a9110d6686efea4c70aaf08c6ba1947ebf2
Alexander <alexander.pe.mobil@gmail.com>
2025-01-24 10:51:22 +0100

    4rd change

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,13 +4,34 @@ public class App {
     
     public static void main(String[] args) {
      
-       
-    }
+        Rectangle rectangle = new Rectangle(10.0, 10.0);
+        Circle circle = new Circle(5.0);
+        Triangle triangle = new Triangle(10.0, 10.0);
 
-  
+        output("------ Flächen ------"); 
+        output("Fläche Rechteck: " + rectangle.area());
+        output("Fläche Kreis: " + circle.area());
+        output("Fläche Dreieck: " + triangle.area());
+
+        // Das geht jetzt nicht mehr ... Abstraktion
+        //Shape shape = new Shape();
+
+        output("------  Differenzen ------");
+        output("Diff. r-t :" + areaDiff(rectangle, triangle));
+        output("Diff. r-c :" + areaDiff(rectangle, circle));
 
    
+    }
 
+    // 1. Iteration : ggf. 1000x überladen
+    private static double areaDiff(Rectangle r, Triangle t){
+        return r.area() - t.area();
+    }
+
+    private static double areaDiff(Rectangle r, Circle c){
+        return r.area() - c.area();
+    }
+    
 
     private static void output(String outputStr) {
         System.out.println(outputStr);
@@ -19,4 +40,3 @@ public class App {
 
 
 }
-
