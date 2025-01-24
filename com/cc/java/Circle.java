4e51d4f794d50adca116e4ac127a4f5e4be7166f
Alexander <alexander.pe.mobil@gmail.com>
2025-01-24 10:52:16 +0100

    5th change

--- com/cc/java/Circle.java
+++ com/cc/java/Circle.java
@@ -1,17 +1,21 @@
 package com.cc.java;
 
-public class Circle {
+public class Circle extends Shape{
 
     private double radius;
 
-    public Circle (double radius) {
+    public Circle(double radius) {
         this.radius = radius;
+    }
 
-   }
-   @Override
-   public double area() {
-    return Math.PI * radius * radius
-   }
+    @Override
+    public double area() {
+        // return Math.PI * radius * radius;
+        return Math.PI * Math.pow(radius, 2);
+    }
 
-   
+
+    
+
+
 }
